1962bbf4e21ffa2c4a77a2fa92ee9e2b3160a4aa
Emre KAÇAN <emrekacan@outlook.com>
2021-06-15 13:37:43 +0300

    initilaized first state configuration

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/root/App";
 import "bootstrap/dist/css/bootstrap.min.css";
+import configureStore from "./redux/reducers/configureStore";
+import { Provider } from "react-redux";
 
+const store = configureStore();
 ReactDOM.render(
-    <React.StrictMode>
+    <Provider store={store}>
         <App />
-    </React.StrictMode>,
+    </Provider>,
     document.getElementById("root")
 );
